2f334e19aef6594e655acac3b5f363790854e9e4
Andru70 <sanandres2504@gmail.com>
2022-02-18 05:48:25 -0500

    .

--- index.php
+++ index.php
@@ -88,6 +88,9 @@
                         <!-- <button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Crear cuenta</button> -->
                         <input type="submit" value="crear cuenta">
                         <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+
+
+
 <!--                         <hr class="my-4">
                         <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
                         <button class="w-100 py-2 mb-2 btn btn-outline-dark rounded-4" type="submit">
@@ -162,14 +165,128 @@
             </div>
         </div>
     </div>
+<!--  -->
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-3 col-lg-4"  style="margin-top: 14px; width: 300px; ">
+            <div class="card">
+             <h4 class="card-title" style="margin:5px auto; font-size:19px;">Increasing Pagina Web</h4>
+             <h7 class="card-title" style="margin:0px auto 4px; font-size:13px;">Tecnología</h7>
+             <img src="./img/Penelope.jpg" alt="Probando..." class="card-img-top"" >
+             <div class="card-body" style="padding-top:9px; padding-bottom:1px;">
+                 <!-- <h5 class="card-title"></h5>   -->
+                 <p class="card-text" style="font-size: 13px;">Penelope sueña con montar su propia panaderia con pan casero, pero actualmente no cuenta con la maquinaria necesaria para hacer los panes, tortas o frituras.</p>
+               
+                 <p style="margin-top:15px; font-size: 13px;">Capital necesario: $17'000.000 COP <br/> Rentabilidad: 18% anual</p>
 
-    <head>
-        <div>
+                 <!-- <p class="card-text" style="margin:2px auto;"><b> Dinero recaudado: </b><br/></p> -->
 
+                 <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 29%;" aria-valuenow="29" aria-valuemin="0" aria-valuemax="100">29%</div>
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 71%" aria-valuenow="29" aria-valuemin="0" aria-valuemax="100">71%</div>
+                 </div>
+                <div style="display:flex;">
+                 <div style="margin:2px auto; font-size:13px; height: 20px;">
+                    <p style="margin:0px;">Recaudado</p>
+                    <p style="margin:0px;"><b>$4'930.000</b></p>
+                </div>
+
+                <div style="margin:0 30px;">
+                    <p style="margin:0px;"><b>   -   </b></p>
+                    <p style="margin:0px;"><b>  -   </b></p>
+                </div>
+
+                <div style="margin:2px auto; font-size:13px;">
+                    <p style="margin:0px;">Por recaudar</p>
+                    <p style="margin:0px;"><b>$12'070.000</b></p>
+                </div>
+                </div>
+                 <!-- <p style="margin:2px auto; font-size:13px;"><b>Recaudado</b>&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp<b>Por recaudar</b> <br/>$4'930.000 COP&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-&nbsp&nbsp&nbsp&nbsp$12'070.000 COP</p> -->
+                 <!-- <div class="progress">
+                    <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                 </div> -->
+             </div>
+            </div>
         </div>
-    </head>
+        <div class="col-md-1"></div>
+        <div class="col-12 col-md-3 col-lg-4"  style="margin-top: 14px; width: 300px; ">
+            <div class="card">
+             <h4 class="card-title" style="margin:5px auto; font-size:19px;">Panadería La Fresca</h4>
+             <h7 class="card-title" style="margin:0px auto 4px; font-size:13px;">Alimentos</h7>
+             <img src="./img/hakimi.jpg" alt="Probando..." class="card-img-top"" >
+             <div class="card-body" style="padding-top:9px; padding-bottom:1px;">
+                 <!-- <h5 class="card-title"></h5>   -->
+                 <p class="card-text" style="font-size: 13px;">La Fresca se especializa en la producción de panes especiales, elaborados de manera artesanal, ademas tendrá panes más innovadores que buscan conseguir un aspecto diferenciador frente a otras panaderías.
+                 </p>
+               
+                 <p style="margin-top:15px; font-size: 13px;">Capital necesario: $7'000.000 COP <br/> Rentabilidad: 10% anual</p>
+
+                 <!-- <p class="card-text" style="margin:2px auto;"><b> Dinero recaudado: </b><br/></p> -->
+
+                 <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 15%;" aria-valuenow="29" aria-valuemin="0" aria-valuemax="100">15%</div>
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 85%" aria-valuenow="29" aria-valuemin="0" aria-valuemax="100">85%</div>
+                 </div>
+                <div style="display:flex;">
+                 <div style="margin:2px auto; font-size:13px; height: 20px;">
+                    <p style="margin:0px;">Recaudado</p>
+                    <p style="margin:0px;"><b>$1'050.000</b></p>
+                </div>
+
+                <div style="margin:0 30px;">
+                    <p style="margin:0px;"><b>   -   </b></p>
+                    <p style="margin:0px;"><b>  -   </b></p>
+                </div>
+
+                <div style="margin:2px auto; font-size:13px;">
+                    <p style="margin:0px;">Por recaudar</p>
+                    <p style="margin:0px;"><b>$5'950.000</b></p>
+                </div>
+                </div>
+             </div>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-12 col-md-3 col-lg-4"  style="margin-top: 14px; width: 300px; ">
+            <div class="card">
+             <h4 class="card-title" style="margin:5px auto; font-size:19px;">Relojes V5</h4>
+             <h7 class="card-title" style="margin:0px auto 4px; font-size:13px;">Accesorios</h7>
+             <img src="./img/lucia.jpg" alt="Probando..." class="card-img-top"" >
+             <div class="card-body" style="padding-top:9px; padding-bottom:1px;">
+                 <!-- <h5 class="card-title"></h5>   -->
+                 <p class="card-text" style="font-size: 13px;">Relojes World es un emprendimiento dedicado a la comercialización al por menor de artículos de relojería, tanto de hombre como de mujer y para cualquier edad. Además, contamos con servicio de reparación de relojes.
+                </p>
+               
+                 <p style="margin-top:15px; font-size: 13px;">Capital necesario: $3'000.000 COP <br/> Rentabilidad: 12.5% anual</p>
+
+                 <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 36%;" aria-valuenow="36" aria-valuemin="0" aria-valuemax="100">36%</div>
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 64%" aria-valuenow="64" aria-valuemin="0" aria-valuemax="100">64%</div>
+                 </div>
+                <div style="display:flex;">
+                 <div style="margin:2px auto; font-size:13px; height: 20px;">
+                    <p style="margin:0px;">Recaudado</p>
+                    <p style="margin:0px;"><b>$1'080.000</b></p>
+                </div>
+
+                <div style="margin:0 30px;">
+                    <p style="margin:0px;"><b>   -   </b></p>
+                    <p style="margin:0px;"><b>  -   </b></p>
+                </div>
+
+                <div style="margin:2px auto; font-size:13px;">
+                    <p style="margin:0px;">Por recaudar</p>
+                    <p style="margin:0px;"><b>$1'920.000</b></p>
+                </div>
+                </div>
+             </div>
+            </div>
+        </div>
+
+    </div>
+ </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
@@ -180,8 +297,5 @@
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
-
-
 </body>
-
 </html>
